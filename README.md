4593ed88b497266f497be9c4c94f2d47177b4779
Mark90098 <markmclaughlin400@gmail.com>
2023-09-10 13:31:06 -0700

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Module-10-SQLAlchemy
+#This code is for module 10 sqlalchemy homework
--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 # Import the dependencies.
+# 1. import Flask
+from flask import Flask
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
+# Save reference to the table
+Passenger = Base.classes.passenger
 
-# Save references to each table
 
 
 # Create our session (link) from Python to the DB
@@ -21,10 +32,28 @@
 #################################################
 # Flask Setup
 #################################################
-
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+
+# 4. Define what to do when a user hits the /about route
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
